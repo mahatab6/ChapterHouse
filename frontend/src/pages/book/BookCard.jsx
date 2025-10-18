f87b6005fc3713a6f4eb375b1537336ccf2467e6
mahatav6 <mahatabcall56@gmail.com>
2025-10-18 19:53:22 +0600

    top seller section done

--- frontend/src/pages/book/BookCard.jsx
+++ frontend/src/pages/book/BookCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FiShoppingCart } from "react-icons/fi";
 import { getimageUrl } from "../../utils/getimageUrl";
+import { Link } from "react-router";
 
 function BookCard({book}) {
     console.log(book)
@@ -10,7 +11,7 @@ function BookCard({book}) {
         <div className="sm:h-72 sm:flex-shrink-0 border rounded-md">
           <a href="/">
             <img
-              src={`${getimageUrl(book.coverImage)}`}
+              src={`${getimageUrl(book?.coverImage)}`}
               alt=""
               className="w-full bg-cover p-2 rounded-md cursor-pointer hover:scale-105 transition-all duration-200"
             />
@@ -18,16 +19,16 @@ function BookCard({book}) {
         </div>
 
         <div>
-          <a href="/">
+          <Link to={`/book/${book?._id}`}>
             <h3 className="text-xl font-semibold hover:text-blue-600 mb-3">
-              {book.title}
+              {book?.title}
             </h3>
-          </a>
-          <p className="text-gray-600 mb-5">{book.description}</p>
+          </Link>
+          <p className="text-gray-600 mb-5">{book.description?.length > 80 ? `${book.description.slice(0,80)}...` : book.description}</p>
           <p className="font-medium mb-5">
-            ${book.newPrice} <span className="line-through font-normal ml-2">${book.oldPrice}</span>
+            ${book?.newPrice} <span className="line-through font-normal ml-2">${book?.oldPrice}</span>
           </p>
-          <button className="btn-primary px-6 space-x-1 flex items-center gap-1 ">
+          <button className="btn-primary px-6 space-x-1 flex items-center gap-1 bg-yellow-300 py-1 rounded-md font-semibold hover:cursor-pointer">
             <FiShoppingCart className="" />
             <span>Add to Cart</span>
           </button>
--- frontend/src/pages/home/TopSellers.jsx
+++ frontend/src/pages/home/TopSellers.jsx
@@ -1,6 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import BookCard from '../book/BookCard';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import required modules
+import { Navigation, Pagination } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+
 
 const categorys = ['choose a genre', 'business', 'marketing', 'horror', 'adventure'];
 
@@ -34,11 +45,42 @@ function TopSellers() {
         </select>
       </div>
 
+       <Swiper navigation={true} modules={[Navigation]}
+        slidesPerView={1}
+        spaceBetween={30}
+        
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Navigation, Pagination]}
+        className="mySwiper"
+      >
+        
       {
         filterBooks.map((book, index) => (
-          <BookCard key={index} book={book} />
+          <SwiperSlide key={index}><BookCard  book={book} /></SwiperSlide>
+          
         ))
       }
+
+        
+      </Swiper>
+
     </div>
   )
 }
