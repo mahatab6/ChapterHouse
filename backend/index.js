80c0d63da03296f90480b17282bd678fd788862d
mahatav6 <mahatabcall56@gmail.com>
2025-10-22 12:25:08 +0600

    mongoose and mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.send('Book server runging!')
-})
+const port = 3000;
+
+async function main() {
+  await mongoose.connect(process.env.DATABASE_URL);
+
+  app.get("/", (req, res) => {
+    res.send("Book server runging!");
+  });
+}
+
+main().then(() => console.log("Mongodb connenct successfully!")).catch(err => console.log(err));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
